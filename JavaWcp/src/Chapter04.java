31dea7f8e0d35a488e1c4ac112055132bbcb3643
ogawahisato <ogawahisato@localhost>
2022-01-08 19:49:02 +0900

    Javaの練習フォルダ

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str1 = "Hisato Ogawa";
+
+		final int LUCKY_NUMBER = 7;
+		System.out.println(str1);
+		System.out.println(LUCKY_NUMBER);
+
+		str1 = "能ある鷹は爪隠す";
+		System.out.println(str1);
+	}
+
+}
---
+++ JavaWcp/src/Chapter07.java
@@ -0,0 +1,17 @@
+
+public class Chapter07 {
+	public static void main(String[] args) {
+		int range = 7;
+		if (range <= 5) {
+			System.out.println("とても近いです");
+		} else if (range <= 10) {
+			System.out.println("近いです");
+		} else if (range <= 15) {
+			System.out.println("遠いです");
+		} else {
+			System.out.println("とても遠いです");
+		}
+	}
+
+}
+
